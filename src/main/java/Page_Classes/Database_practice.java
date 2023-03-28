ec8b981023e774fe2d3655938eff12547f2cf9de
nj839470 <nj839470@gmail.com>
2023-03-28 17:57:46 +0530

    .

--- src/main/java/Page_Classes/Database_practice.java
+++ src/main/java/Page_Classes/Database_practice.java
@@ -7,13 +7,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Database_practice {
-	static String name ="" ,des ="";
-	static int age =0 , rollnum =0 ,salary =0;
+	static String name ="" ,des ="" , age ="",rollnum ="",salary ="";
+	//static int age =0 , rollnum =0 ,salary =0;
 	 ResultSet result;
 	public ResultSet readdata(String query)
 	{
 		try {
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/nk","root","Sql@12345");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys","root","Sql@12345");
 			PreparedStatement statement = con.prepareStatement(query);
 			 result = statement.executeQuery();
 			} catch (Exception e) {
@@ -23,21 +23,22 @@ public class Database_practice {
 	}
 	public static void main(String[] args) throws SQLException {
 		Database_practice ob = new Database_practice();
-		ResultSet res = ob.readdata("delete employee");
+		ResultSet res = ob.readdata("select * from sys_config");
+		System.out.println(res);
 		while(res.next())
 		{
 			int i=1;
 			 name= res.getString(i);
-//			 i++;
-//			 age = res.getInt(i);
-//			 i++;
-//			 rollnum = res.getInt(i);
-//			 i++;
-//			 salary = res.getInt(i);
-//			 i++;
-//			 des = res.getString(i);
-//			 System.out.println(rollnum + "   " + name+ "   " + age +"   "+ salary+ "   " +des +"\n");
-			 System.out.println(name);
+			 i++;
+			 age = res.getString(i);
+			 i++;
+			 rollnum = res.getString(i);
+			 i++;
+			 salary = res.getString(i);
+			 i++;
+			// des = res.getString(i);
+			 System.out.println(rollnum + "   " + name+ "   " + age +"   "+ salary+ "   " +"\n");
+			// System.out.println(name);
 		}
 		
 	}
